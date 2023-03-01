a2938899f03e5e9755326bb6656b4ae9020297fc
Hunter Heston <huntersamuelheston@gmail.com>
2023-03-01 15:29:00 -0800

    add echo web framework dependency
    https://echo.labstack.com/

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Echo instance
+	e := echo.New()
+
+	// Routes and handlers
+	e.POST("/record", handleRecord)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":3001"))
+}
+
+/////////////////
+// handler logic
+/////////////////
+
+type RecordInformation struct {
+	RecordType    string    `json:"RecordType"`
+	Type          string    `json:"Type"`
+	TypeCode      int       `json:"TypeCode"`
+	Name          string    `json:"Name"`
+	Tag           string    `json:"Tag"`
+	MessageStream string    `json:"MessageStream"`
+	Description   string    `json:"Description"`
+	Email         string    `json:"Email"`
+	From          string    `json:"From"`
+	BouncedAt     time.Time `json:"BouncedAt"`
+}
+
+// Handle POST requests and optionally send a message to a Slack channel
+func handleRecord(c echo.Context) error {
+	recordInfo := RecordInformation{}
+	if err := c.Bind(&recordInfo); err != nil {
+		return err
+	}
+	return c.JSON(200, recordInfo)
 }
