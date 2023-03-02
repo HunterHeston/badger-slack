d824348cf3567e05f00ba43a00ee084e94448433
Hunter Heston <huntersamuelheston@gmail.com>
2023-03-01 17:44:26 -0800

    implement SendMessageToSlack 1. read SLACK_API_TOKEN and CHANNEL from env 2. use slack-go to send a message to slack.

--- slack/sendMessage.go
+++ slack/sendMessage.go
@@ -2,9 +2,47 @@ package slack
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
-func SendMessageToSlack(channel, message string) error {
-	fmt.Println("Sending message to Slack channel!!!")
+// client for interacting with the Slack API
+var client *slack.Client
+
+// specific channel in slack to send messages to
+var channel string
+
+func init() {
+	// read in the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// grab values from the .env file
+	token := os.Getenv("SLACK_API_TOKEN")
+	if token == "" {
+		log.Fatal("SLACK_API_TOKEN is not set")
+	}
+	notificationChanel := os.Getenv("CHANNEL")
+	if notificationChanel == "" {
+		log.Fatal("CHANNEL is not set")
+	}
+
+	// use the values or save for later
+	client = slack.New(token)
+	channel = notificationChanel
+}
+
+// SendMessageToSlack sends the provided message to a Slack channel
+func SendMessageToSlack(message string) error {
+	_, _, err := client.PostMessage(channel, slack.MsgOptionText(message, false))
+	if err != nil {
+		return fmt.Errorf("failed to post: %v", err)
+	}
+
 	return nil
 }
